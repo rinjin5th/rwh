1dae5fd38ba3d5978f0d5c778f51ba72b79a8e76
rinjin5th <imasa510@gmail.com>
2018-11-13 01:19:11 +0900

    add chapter3

---
+++ ch3/head/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Head("http://localhost:18888")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status:", resp.Status)
+	log.Println("Headers:", resp.Header)
+}
